39860f067a3f9c97b8eef8d20b252856c4e317f7
mikemsrk <min2033@gmail.com>
2015-08-24 19:58:38 -0700

    Initial Commit.

---
+++ Assets/Grid.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	public LayerMask unwalkableMask;
+	public Vector2 gridWorldSize;
+	public float nodeRadius;
+	
+	Node[,] grid; // 2D array of nodes
+	
+	// Use gizmos to visualize
+	void OnDrawGizmos(){
+		Gizmos.DrawWireCube (transform.position,new Vector3(gridWorldSize.x,1,gridWorldSize.y));
+		
+	
+	
+	}
+		
+	
+}
